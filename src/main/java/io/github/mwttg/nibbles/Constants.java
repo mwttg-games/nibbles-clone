c35cc5397d2137a06219c47e7780b1e08c36c209
Marco Wittig <marco-wittig@gmx.de>
2025-04-25 19:37:12 +0200

    Format Commit

--- src/main/java/io/github/mwttg/nibbles/Constants.java
+++ src/main/java/io/github/mwttg/nibbles/Constants.java
@@ -2,11 +2,11 @@ package io.github.mwttg.nibbles;
 
 public interface Constants {
 
-    // OpenGL Misc
-    int WIDTH = 1920;
-    int HEIGHT = 1080;
-    float NEAR_PLANE = 0.01f;
-    float FAR_PLANE = 301.0f;
-    int TILE_WIDTH = 16;
-    int TILE_HEIGHT = 18;
+  // OpenGL Misc
+  int WIDTH = 1920;
+  int HEIGHT = 1080;
+  float NEAR_PLANE = 0.01f;
+  float FAR_PLANE = 301.0f;
+  int TILE_WIDTH = 16;
+  int TILE_HEIGHT = 18;
 }
