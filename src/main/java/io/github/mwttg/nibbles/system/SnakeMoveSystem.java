4bc11cc0d74c35930b42eade03345604a067b0d8
Marco Wittig <marco-wittig@gmx.de>
2025-05-23 19:54:02 +0200

    Fix Bug of 180 degree turns

--- src/main/java/io/github/mwttg/nibbles/system/SnakeMoveSystem.java
+++ src/main/java/io/github/mwttg/nibbles/system/SnakeMoveSystem.java
@@ -28,16 +28,35 @@ public final class SnakeMoveSystem {
 
     Assets.getInstance().getSoundMove().play();
     snakeEntity.resetDeltaTime();
-    snakeEntity.setDirection(direction);
+    final Direction correctedDirection = directionCorrection(snakeEntity.getDirection(), direction);
+    snakeEntity.setDirection(correctedDirection);
 
     final Position oldHead = snakeEntity.getHead();
-    final Position newHead = moveHead(direction, oldHead);
+    final Position newHead = moveHead(correctedDirection, oldHead);
 
     final List<Position> oldTail = snakeEntity.getTail();
     final List<Position> newTail = moveTail(oldHead, oldTail);
 
     snakeEntity.setHead(newHead);
     snakeEntity.setTail(newTail);
+  }
+
+  // avoid 180 degree turns
+  private static Direction directionCorrection(final Direction oldDirection, final Direction newDirection) {
+    if (oldDirection == Direction.UP && newDirection == Direction.DOWN) {
+      return Direction.UP;
+    }
+    if (oldDirection == Direction.DOWN && newDirection == Direction.UP) {
+      return Direction.DOWN;
+    }
+    if (oldDirection == Direction.LEFT && newDirection == Direction.RIGHT) {
+      return Direction.LEFT;
+    }
+    if (oldDirection == Direction.RIGHT && newDirection == Direction.LEFT) {
+      return Direction.RIGHT;
+    }
+
+    return newDirection;
   }
 
   private static Position moveHead(final Direction direction, final Position head) {
